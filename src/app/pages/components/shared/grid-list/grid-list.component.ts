1ac99da7181a230d64dc017d587a0a5f9db8f460
Ammar  Khalifa <a.m.k-it@hotmail.com>
2024-10-22 20:47:00 +0400

    grid first scenario

--- src/app/pages/components/shared/grid-list/grid-list.component.ts
+++ src/app/pages/components/shared/grid-list/grid-list.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 @Component({
   selector: 'app-grid-list',
   standalone: true,
-  imports: [NzCardModule, NzDividerModule, NzDescriptionsModule, NzButtonModule],
+  imports: [
+    NzCardModule,
+    NzDividerModule,
+    NzDescriptionsModule,
+    NzButtonModule,
+    NzIconModule,
+    NzSpaceModule,
+  ],
   templateUrl: './grid-list.component.html',
   styleUrl: './grid-list.component.css',
 })
